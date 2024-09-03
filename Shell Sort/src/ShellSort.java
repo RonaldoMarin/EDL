e3935e0194122f65a99c17be3874cecd5e148a30
ronaldoMarin <marinhoronaldo02@gmail.com>
2024-09-03 07:22:37 -0300

    feat: adição de metodo para leitura dos elementos do array.

--- Shell Sort/src/ShellSort.java
+++ Shell Sort/src/ShellSort.java
@@ -14,16 +14,26 @@ public class ShellSort {
         }
     }
 
-    public static void main(String[] args) {
-        // int[] nums = {1, 12, 312, 4, 0, -2, 22, 13, 4};
-        int[] vetor = new int[10];
-        System.out.println("Vetor desordenado");
-        for (int i = 0; i < vetor.length; i++){
-            vetor[i] = (int) (Math.random() * vetor.length);
-            System.out.print(vetor[i] + " ");
+    public static void printArray(int[] array){
+        for (int i = 0; i < array.length; i++){
+            System.out.print(array[i] + " ");
         }
+    }
+
+    public static void main(String[] args) {
+        int[] vetor = {9, 8, 3, 7, 5, 6, 4, 1};
+        System.out.println("Vetor desordenado");
+        printArray(vetor);
+
+        // int[] vetor = new int[10];
+        // for (int i = 0; i < vetor.length; i++){
+        //     vetor[i] = (int) (Math.random() * vetor.length);
+        //     System.out.print(vetor[i] + " ");
+        // }
+
         ShellSort.sort(vetor);
-        System.out.println("\n");
+        System.out.println("\nVetor ordenado");
+
         // Imprimir o array ordenado
         for (int num : vetor) {
             System.out.print(num + " ");
